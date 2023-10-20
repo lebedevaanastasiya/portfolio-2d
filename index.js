aa276f4b895915a5dea3bde4605a8c6700616699
lebedeanastasija <lebedeanastasija@yandex.by>
2023-10-20 13:35:46 +0200

    Add platform & fix texture loading

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const app = new PIXI.Application({
 });
 document.body.append(app.view);
 
-// create player and position on on the scene
-const player = PIXI.Sprite.from("./assets/player.jpg");
+// Create player and position it on the scene
+const playerTexture = await PIXI.Assets.load("./assets/player.jpg");
+const player = PIXI.Sprite.from(playerTexture);
 player.anchor.set(0.5);
 player.x = app.screen.width / 2;
 player.y = app.screen.height / 2;
@@ -49,6 +50,31 @@ function onArrowLeftTouchStart() {
 function onArrowLeftTouchEnd() {
   onKeyUp({ keyCode: 37 });
 }
+
+// Create game platform
+const groundTexture = await PIXI.Assets.load("./assets/ground.jpg");
+const grassTexture = await PIXI.Assets.load("./assets/grass.jpg");
+const TILE_SIZE = groundTexture.width;
+const PLATFORM_ROWS = 4;
+const PLATFORM_COLUMNS = Math.ceil(window.innerWidth / TILE_SIZE);
+const platformWidth = TILE_SIZE * PLATFORM_COLUMNS;
+const platformHeight = TILE_SIZE * PLATFORM_ROWS;
+const platform = new PIXI.Container();
+platform.x = 0;
+platform.y = window.innerHeight - platformHeight;
+
+for (let row = 0; row < PLATFORM_ROWS; row++) {
+  for (let col = 0; col < PLATFORM_COLUMNS; col++) {
+    const texture = row === 0 ? grassTexture : groundTexture;
+    const tile = PIXI.Sprite.from(texture);
+    platform.addChild(tile);
+    tile.x = col * tile.width;
+    tile.y = row * tile.height;
+    console.log("tile", tile);
+  }
+}
+
+app.stage.addChild(platform);
 
 // Main game loop
 app.ticker.add(gameLoop);
